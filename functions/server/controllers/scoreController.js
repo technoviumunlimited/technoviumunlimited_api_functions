661b4a89db675dde173779a4a2ecfa28b2f9ff59
binerystorm <gerick.de.kruyf123@gmail.com>
2022-12-15 12:03:58 +0100

    added level_data end point

--- functions/server/controllers/scoreController.js
+++ functions/server/controllers/scoreController.js
@@ -14,3 +14,33 @@ exports.getLevelsOfGame = async (req, res, next) => {
 		res.status(500).send();
 	}
 };
+
+exports.getLevelData = async (req, res, next) => {
+	let paramID = req.params.game_id;
+	try {
+		score_users = [];
+		level_datas = [];
+
+		const query = await db.collection("games").doc(paramID)
+							  .collection("levels").doc("1")
+							  .collection("score_users").orderBy("started", "desc").limit(1).get()
+		query.forEach((score_user) => score_users.push({...score_user.data(), _id: score_user.id}));
+
+		console.log(score_users[0]._id)
+
+		const query2 = await db.collection("games").doc(paramID)
+							  .collection("levels").doc("1")
+							  .collection("score_users")
+							  .doc(score_users[0]._id)
+							  .collection("level_data")
+							  .orderBy("created", "desc").limit(1).get();
+		
+		query2.forEach((level_data) => level_datas.push({...level_data.data(), bin: level_data.binary}));
+		binary = level_datas[0].binary;
+		res.status(200).json({binary});
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).send();
+	}
+}
--- functions/server/routes/routes.js
+++ functions/server/routes/routes.js
@@ -11,5 +11,6 @@ router.get("/api/"+ process.env.API_VERSION +"/games", gameController.getGames);
 router.get("/api/"+ process.env.API_VERSION +"/level/:game_id/levels", scoreController.getLevelsOfGame);
 router.get("/api/"+ process.env.API_VERSION +"/level/:game_id", levelController.getLevelsOfGame);
 router.get("/api/"+ process.env.API_VERSION +"/user/:user_id", authMiddleware, userController.getallUsers);
+router.get("/" + process.env.API_VERSION +"/score/:game_id/users/level_data", scoreController.getLevelData)
 
 module.exports = router;
