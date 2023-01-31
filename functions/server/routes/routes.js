406ae224228e8ac443797f11c2ae450d4a89515c
dakanadaka <113799440+EnergeticBubble@users.noreply.github.com>
2023-01-31 13:48:05 +0100

    Update routes.js

--- functions/server/routes/routes.js
+++ functions/server/routes/routes.js
@@ -11,7 +11,7 @@ router.get("/"+ process.env.API_VERSION +"/games", gameController.getGames);
 router.get("/"+ process.env.API_VERSION +"/level/:game_id/levels/:level_id/", scoreController.getLevelsOfGame);
 router.get("/"+ process.env.API_VERSION +"/level/:game_id", levelController.getLevelsOfGame);
 router.get("/"+ process.env.API_VERSION +"/user/:user_id", authMiddleware, userController.getallUsers);
-router.get("/" + process.env.API_VERSION +"/score/:game_id/users/level_data", authMiddleware, scoreController.getLevelData)
+router.get("/" + process.env.API_VERSION +"/score/:game_id/users/level_data", scoreController.getLevelData)
 router.post("/" + process.env.API_VERSION +"/score/insert", authMiddleware, scoreController.insertLevelUserScoreData)
 router.post("/" + process.env.API_VERSION +"/score/insert/finished", authMiddleware, scoreController.insertLevelUserScoreDataFinished)
 module.exports = router;
