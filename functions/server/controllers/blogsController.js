16d3d3aba7e26252b94088d415a32aee109ac9e7
jaspervanuden <jaspervanuden03@gmail.com>
2023-10-03 11:29:10 +0200

    blogs

---
+++ functions/server/controllers/blogsController.js
@@ -0,0 +1,41 @@
+const { db, storage } = require("../models/db");
+
+exports.getBlogs = async (req, res, next) => {
+  try {
+    const options = {
+      version: "v4",
+      action: "read",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    };
+
+    const query = await db
+      .collection("blogs")
+      .where("active", "==", true)
+      .orderBy("position")
+      .get();
+    const data = query.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    const blogs = await Promise.all(
+      data.map(async (blog) => {
+        const thumb = await storage
+          .bucket("technoviumunlimited.appspot.com")
+          .file("blogs/" + blog.id + "/thumb.png")
+          .getSignedUrl(options);
+
+        return new Promise((resolve, reject) => {
+          return resolve({
+            _id: blog.id,
+            _thumb: thumb,
+            title: blog.title,
+            discription: blog.discription,
+            author: blog.author,
+            position: blog.position,
+          });
+        });
+      })
+    );
+    res.status(200).json({ blogs });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+};
--- functions/server/routes/routes.js
+++ functions/server/routes/routes.js
@@ -3,16 +3,43 @@ const gameController = require("../controllers/gameController");
 const levelController = require("../controllers/levelController");
 const scoreController = require("../controllers/scoreController");
 const userController = require("../controllers/userController");
+const blogsController = require("../controllers/blogsController");
 
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/"+ process.env.API_VERSION +"/games", gameController.getGames);
-router.get("/"+ process.env.API_VERSION +"/games/:game_id", gameController.getGame);
-router.get("/"+ process.env.API_VERSION +"/level/:game_id/levels/:level_id/", scoreController.getLevelsOfGame);
-router.get("/"+ process.env.API_VERSION +"/level/:game_id", levelController.getLevelsOfGame);
-router.get("/"+ process.env.API_VERSION +"/user/:user_id", authMiddleware, userController.getallUsers);
-router.get("/" + process.env.API_VERSION +"/score/:game_id/users/level_data", scoreController.getLevelData)
-router.post("/" + process.env.API_VERSION +"/score/insert", authMiddleware, scoreController.insertLevelUserScoreData)
-router.post("/" + process.env.API_VERSION +"/score/insert/finished", authMiddleware, scoreController.insertLevelUserScoreDataFinished)
+router.get("/" + process.env.API_VERSION + "/games", gameController.getGames);
+router.get(
+  "/" + process.env.API_VERSION + "/games/:game_id",
+  gameController.getGame
+);
+router.get(
+  "/" + process.env.API_VERSION + "/level/:game_id/levels/:level_id/",
+  scoreController.getLevelsOfGame
+);
+router.get(
+  "/" + process.env.API_VERSION + "/level/:game_id",
+  levelController.getLevelsOfGame
+);
+router.get(
+  "/" + process.env.API_VERSION + "/user/:user_id",
+  authMiddleware,
+  userController.getallUsers
+);
+router.get(
+  "/" + process.env.API_VERSION + "/score/:game_id/users/level_data",
+  scoreController.getLevelData
+);
+router.post(
+  "/" + process.env.API_VERSION + "/score/insert",
+  authMiddleware,
+  scoreController.insertLevelUserScoreData
+);
+router.post(
+  "/" + process.env.API_VERSION + "/score/insert/finished",
+  authMiddleware,
+  scoreController.insertLevelUserScoreDataFinished
+);
+router.get("/" + process.env.API_VERSION + "/blogs", blogsController.getBlogs);
+
 module.exports = router;
